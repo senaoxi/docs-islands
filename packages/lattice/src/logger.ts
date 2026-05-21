4a43e4a191efe238e859049a62d6691f3566f356
SenaoXi <email@senao.me>
2026-05-21 09:57:23 +0800

    chore: type supplement

--- packages/lattice/src/logger.ts
+++ packages/lattice/src/logger.ts
@@ -1,15 +1,18 @@
 import { createLogger } from '@docs-islands/logger';
 import { formatErrorMessage } from '@docs-islands/logger/helper';
+import type { ScopedLogger } from '@docs-islands/logger/types';
 
 const logger = createLogger({
   main: '@docs-islands/lattice',
 });
 
-export const CliLogger = logger.getLoggerByGroup('task.cli');
-export const GraphLogger = logger.getLoggerByGroup('task.graph');
-export const PackageLogger = logger.getLoggerByGroup('task.package');
-export const PathsLogger = logger.getLoggerByGroup('task.paths');
-export const ProofLogger = logger.getLoggerByGroup('task.proof');
-export const TypecheckLogger = logger.getLoggerByGroup('task.typecheck');
+export const CliLogger: ScopedLogger = logger.getLoggerByGroup('task.cli');
+export const GraphLogger: ScopedLogger = logger.getLoggerByGroup('task.graph');
+export const PackageLogger: ScopedLogger =
+  logger.getLoggerByGroup('task.package');
+export const PathsLogger: ScopedLogger = logger.getLoggerByGroup('task.paths');
+export const ProofLogger: ScopedLogger = logger.getLoggerByGroup('task.proof');
+export const TypecheckLogger: ScopedLogger =
+  logger.getLoggerByGroup('task.typecheck');
 
 export { formatErrorMessage };
